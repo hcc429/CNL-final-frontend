6df7e322f89ee3d8385ee10f58073a91867655ac
spockwall <abcdefg35216874@gmail.com>
2023-05-28 01:54:56 +0800

    fix login bug

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import main from "../assets/main.png";
 import LoginForm from "../forms/LoginForm";
+import useAuth from "../hooks/useAuth";
 
 export default function Login(): JSX.Element {
+	const auth = useAuth(false);
+	useEffect(() => {
+		if (auth) {
+			window.location.href = "/";
+		}
+	}, []);
 	return (
 		<div className="flex items-center w-9/12 m-auto text-center h-200 mt-20">
 			<div className="flex items-center bg-contain">
